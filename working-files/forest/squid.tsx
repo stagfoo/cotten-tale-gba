78808c6059e5b0f7edba43113e4e99b15460bdff
stagfoo <alexanderddking@gmail.com>
2022-04-06 09:21:12 +0900

    🪴　feature ░ added lilly and vine

--- working-files/forest/squid.tsx
+++ working-files/forest/squid.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.2" name="squid" tilewidth="8" tileheight="8" tilecount="224" columns="14">
- <image source="squid.png" width="119" height="131"/>
+<tileset version="1.8" tiledversion="1.8.2" name="squid" tilewidth="8" tileheight="8" tilecount="240" columns="15">
+ <image source="squid.png" width="120" height="131"/>
 </tileset>
